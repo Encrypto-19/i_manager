3661f0b6dae370a00b342d97fde3f4216cc1c701
encrypto <4545encrypto5454@gmail.com>
2020-05-25 01:56:09 +0530

    hid django secret key

--- i_manager/settings.py
+++ i_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+vd8w!kn2=qej+ad1q&q^m)1qfba8l(b*dq#hkb!$=k#^iru7#'
+SECRET_KEY = os.environ.get('I_MANAGER_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
